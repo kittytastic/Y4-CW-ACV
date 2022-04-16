64c252a5a00f9294c01da13a1d6b3aeac77705cc
Jack Slingsby <jack@theslingsbys.com>
2022-04-16 18:52:53 +0100

    Polishes up human extract

--- Solution/human_patch_extract.py
+++ Solution/human_patch_extract.py
@@ -1,17 +1,13 @@
 from typing import Any, List
-import cv2
 import numpy as np
-import random
 import torch
 import torchvision
-from PIL import Image
-from IPython import display
-from torchvision.transforms import transforms
 from Helpers.video import VideoReader
 from Helpers.video_loader import VideoLoader
 from Helpers.image_dir import ImageDirWriter
-from Helpers.images import tensor_to_openCV
 from tqdm import tqdm
+import argparse
+import os.path
 
 # Adapted from: https://colab.research.google.com/drive/1bWLB3tmWv4XyJSu4DHtZ0-b-n-DACSKx?usp=sharing
 class MaskRCNN():
@@ -80,18 +76,71 @@ def init_torch()->torch.device:
     
     return device
 
-if __name__=="__main__":
-    print("------ Human Patch Extract ------")
-    batch_size = 2
-    workers= 0
+
+def full_mode(workers:int, batch_size: int):
     device = init_torch()
     mask_rcnn = MaskRCNN(device)
-    video = VideoReader("../Dataset/Train/Games/Video1.mp4")
-    outstream = ImageDirWriter("../Dataset/Generated/HumanPatches")
-    ds = VideoLoader('../Dataset/Train/Games/Video1.mp4')
-    data = torch.utils.data.DataLoader(ds, num_workers=workers, batch_size=batch_size)
-    
 
-    for tensor, opencv  in tqdm(data, total = len(video)//batch_size):
-        people = mask_rcnn.get_humans(tensor, opencv)
-        outstream.write_frames(people)
+    DATA_BASE_PATH = "../Dataset/Train"
+    game_videos = ["Video1.mp4", "Video2.mp4"]
+    movie_videos = ["Video1.mp4", "Video2.mp4", "Video3.mp4", "Video4.mp4", "Video5.mp4", "Video6.mp4", "Video7.mp4", "Video8.mp4", "Video9.mp4"]
+
+
+    game_outstream = ImageDirWriter("../Dataset/Generated/HumanPatches/Games")
+    movie_outstream = ImageDirWriter("../Dataset/Generated/HumanPatches/Movie")
+    
+    total_samples = 0
+    print(f"Processing {len(game_videos)} game videos")
+    for idx, game in enumerate(game_videos):
+        print(f"\tStarting: {game}   ({idx+1})")
+        video_path = os.path.join(DATA_BASE_PATH, "Games", game)
+        video = VideoReader(video_path)
+        ds = VideoLoader(video_path)
+        data = torch.utils.data.DataLoader(ds, num_workers=workers, batch_size=batch_size)
+        samples = 0
+        
+        for tensor, opencv  in tqdm(data, total = len(video)//batch_size):
+            people = mask_rcnn.get_humans(tensor, opencv)
+            samples += len(people)
+            game_outstream.write_frames(people)
+        
+        print(f"\t\t{samples} samples")
+        total_samples += samples
+    print(f"Total samples: {total_samples}")
+    print()
+    total_samples = 0
+    print(f"Processing {len(movie_videos)} movie videos")
+    for idx, movie in enumerate(movie_videos):
+        print(f"\tStarting: {movie}   ({idx+1})")
+        video_path = os.path.join(DATA_BASE_PATH, "Movie", movie)
+        video = VideoReader(video_path)
+        ds = VideoLoader(video_path)
+        data = torch.utils.data.DataLoader(ds, num_workers=workers, batch_size=batch_size)
+        samples = 0
+        
+        for tensor, opencv  in tqdm(data, total = len(video)//batch_size):
+            people = mask_rcnn.get_humans(tensor, opencv)
+            samples += len(people)
+            game_outstream.write_frames(people)
+        
+        print(f"\t\t{samples} samples")
+        total_samples += samples
+    print(f"Total samples: {total_samples}")
+
+if __name__=="__main__":
+    print("------ Human Patch Extract ------")
+    
+    parser = argparse.ArgumentParser(description='Extract Human Patches')
+    parser.add_argument('-b', '--batch', type=int,help='Batch Size', default=16)
+    parser.add_argument('-w', '--workers', type=int, help='Number of Workers', default=4)
+    parser.add_argument('-m', '--mode', type=str, help='Operation Mode', default="full")
+    args = parser.parse_args()
+
+    batch_size = args.batch
+    workers = args.workers
+
+    if args.mode == "full":
+        full_mode(workers, batch_size)
+    else:
+        print(f"Unknown mode: {args.mode}")
+    
