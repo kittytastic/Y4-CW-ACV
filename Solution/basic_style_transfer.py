13273150f415765830727026283ae7363e1db211
Jack Slingsby <jack@theslingsbys.com>
2022-04-22 12:37:23 +0100

    Tidy up basic style transfer

--- Solution/basic_style_transfer.py
+++ Solution/basic_style_transfer.py
@@ -9,10 +9,14 @@ from pytorch_CycleGAN_and_pix2pix.options.test_options import TestOptions
 from pytorch_CycleGAN_and_pix2pix.util.visualizer import save_images
 from pytorch_CycleGAN_and_pix2pix.util import html
 from Helpers.video_loader import VideoLoader
-from Helpers.video import IOBase
+from Helpers.video import IOBase, VideoWriter, VideoReader
+from Helpers.images import tensor_to_openCV, openCV_to_tensor
 
 def adjust_color(in_image):
     return (in_image*2.0)-1
+
+def fix_colour_from_cgan(in_image):
+    return (in_image+1)/2
 
 if __name__=="__main__":
     print("---------- Basic Style Transfer ---------")
@@ -39,7 +43,9 @@ if __name__=="__main__":
     ds = VideoLoader("../Dataset/Train/Games/Video1.mp4", user_transform=adjust_color)
     dataset = torch.utils.data.DataLoader(ds, num_workers=opt.num_threads, batch_size=opt.batch_size) # type: ignore
     samples = 0
+    input_meta = VideoReader("../Dataset/Train/Games/Video1.mp4")
 
+    wr = VideoWriter("../Dataset/TMP/tmp.mp4", like_video=input_meta)
 
     #dataset = create_dataset(opt)  # create a dataset given opt.dataset_mode and other options
 
@@ -48,23 +54,18 @@ if __name__=="__main__":
     for i, data in enumerate(dataset):
         if i >= opt.num_test:  # only apply our model to opt.num_test images.
             break
-        #print(data[0].shape)
-        #print(data[1].shape)
-        cgan_data = {"A": data[0], "A_paths":["nopath"]}
-        #print(data[0].numpy().shape)
-        #print(data[1].numpy().shape)
-        #IOBase.view_frame(data[0][0].numpy())
-        #IOBase.view_frame(data[1][0].numpy())
-        #print(data)
-        #print(data["A"].shape)
+        tensor_image, numpy_image = data[0], data[1]
+        cgan_data = {"A": tensor_image, "A_paths":["nopath"]}
         model.set_input(cgan_data)  # unpack data from data loader
         model.test()           # run inference
         visuals = model.get_current_visuals()  # get image results
-        #print(visuals)
         img_path = model.get_image_paths()     # get image paths
-        #print(img_path)
         if i % 5 == 0:  # save images to an HTML file
             print('processing (%04d)-th image... %s' % (i, img_path))
         save_images(webpage, visuals, img_path, aspect_ratio=opt.aspect_ratio, width=opt.display_winsize, use_wandb=opt.use_wandb)
-        #exit()
+        fake_image = fix_colour_from_cgan(visuals["fake"]).detach().cpu().squeeze()
+        fake_image = tensor_to_openCV(fake_image)
+        fake_image = fake_image[0:wr.height]
+        wr.write_frame(fake_image)
+    
     webpage.save()  # save the HTML
