9dc3b9322402944d74269f3683d2330a150395b2
= <jack@theslingsbys.com>
2022-04-29 21:57:19 +0100

    Updates Q2.3 train

--- Solution/Q2.3_train.py
+++ Solution/Q2.3_train.py
@@ -7,12 +7,13 @@ from Helpers.image_resize_loader import ImageStandardizeDataLoader, ImageStandar
 from Helpers.ab_loader import Custom_AB_Loader, Aligned_Class_Unaligned_Data_AB_Loader
 from Helpers.images import tensor_to_openCV, openCV_to_tensor
 from Helpers.cgan import tensor_to_cycle_gan_colour, cycle_gan_to_tensor_colour, custom_cgan_train, inject_time_arg
-
+from Models.augmentation import patch_tf
 GLOBAL_image_standardizer= ImageStandardizer(256, 256)
 
 def image_loader_tf(img):
-    img, _ = GLOBAL_image_standardizer.standardize_size(img)
-    img = openCV_to_tensor(img)
+    #img, _ = GLOBAL_image_standardizer.standardize_size(img)
+    #img = openCV_to_tensor(img)
+    img = patch_tf(img)    
     img = tensor_to_cycle_gan_colour(img)
     return img
 
@@ -24,10 +25,10 @@ def inject_mode_arg(opt):
 if __name__=="__main__":
     opt = TrainOptions().parse()   # get training options
     
-    mode = inject_mode_arg(opt)
+    #mode = inject_mode_arg(opt)
     opt, target_hrs = inject_time_arg(opt)
 
-    GLOBAL_image_standardizer.resize_mode=mode 
+    #GLOBAL_image_standardizer.resize_mode=mode 
 
     dl = Aligned_Class_Unaligned_Data_AB_Loader("../Dataset/Generated/Poses", "game-output-{id}.jpg", "movie-output-{id}.jpg", tf=image_loader_tf)
     ds = torch.utils.data.DataLoader(dl, num_workers=opt.num_threads, batch_size=opt.batch_size, shuffle=True) # type: ignore
